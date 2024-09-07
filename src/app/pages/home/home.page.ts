bf1a22a873a40323db2b43298f84470d22e15562
Mayckel Zepeda <mayckelzc@gmail.com>
2024-09-06 21:32:21 -0400

    borrado-duplicados

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -8,14 +8,14 @@ import { Router } from '@angular/router';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements AfterViewInit {
-  dogImages: string[] = [
+  readonly dogImages: string[] = [
     'assets/imgs/pixelart-bird.gif',
     'assets/imgs/pixelart-cat.png',
     'assets/imgs/pixelart-dog.png',
   ];
 
   selectedFilter: string = 'all';
-  allPets = [
+  readonly allPets = [
     { name: 'Rex', type: 'dog', age: 3, description: 'Rex es un perro amigable y enérgico que busca un hogar amoroso. ¡Adóptalo y dale una segunda oportunidad!', image: 'https://th.bing.com/th/id/R.6b2026c15d2cc7b6c6453118b8758eb8?rik=p5hZWemaKDXfVg&riu=http%3a%2f%2fwww.razasdeperros.com%2fwp-content%2fuploads%2f2013%2f10%2fDepositphotos_8405161_m.jpg&ehk=076xT4EInAZGdk61vPke7foOGNpYlmbQT2JXi%2fSiMIw%3d&risl=&pid=ImgRaw&r=0' },
     { name: 'Misty', type: 'cat', age: 2, description: 'Misty es una gata tranquila y cariñosa que adora las siestas al sol. ¡Dale la oportunidad de ser parte de tu familia!', image: 'https://th.bing.com/th/id/OIP.B-GtznvQ82H9zcQO7FQs2QHaE8?rs=1&pid=ImgDetMain' },
     { name: 'Polly', type: 'other', age: 1, description: 'Polly es una cotorra juguetona y habladora. ¡Haz que su alegría sea parte de tu hogar!', image: 'https://th.bing.com/th/id/OIP.blLe3ZV9JfNWshN5ikwsbQHaEK?rs=1&pid=ImgDetMain' }
@@ -31,7 +31,7 @@ export class HomePage implements AfterViewInit {
 
   ngAfterViewInit() {
     this.createDogAnimation();
-    this.username = localStorage.getItem('username') || 'Invitado'; // Recupera el nombre de usuario
+    this.username = localStorage.getItem('username') || 'Invitado';
   }
 
   createDogAnimation() {
