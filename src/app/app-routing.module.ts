24b71061ff98cf989daaab7aa8afa6f3c4e151f1
Mayckel Zepeda <mayckelzc@gmail.com>
2024-10-15 17:38:21 -0300

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';  // Importa el guard como función
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
+    canActivate: [authGuard]  // Protege la ruta usando 'authGuard'
   },
   {
     path: '',
@@ -13,23 +15,25 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'detalle',
-    loadChildren: () => import('./pages/detalle/detalle.module').then( m => m.DetallePageModule)
+    loadChildren: () => import('./pages/detalle/detalle.module').then(m => m.DetallePageModule),
+    canActivate: [authGuard]  // Protege la ruta 'detalle'
   },
   {
     path: 'registro',
-    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+    loadChildren: () => import('./pages/registro/registro.module').then(m => m.RegistroPageModule)
   },
   {
     path: 'restablecer',
-    loadChildren: () => import('./pages/restablecer/restablecer.module').then( m => m.RestablecerPageModule)
+    loadChildren: () => import('./pages/restablecer/restablecer.module').then(m => m.RestablecerPageModule)
   },
   {
     path: 'crearadopcion',
-    loadChildren: () => import('./pages/crearadopcion/crearadopcion.module').then( m => m.CrearadopcionPageModule)
+    loadChildren: () => import('./pages/crearadopcion/crearadopcion.module').then(m => m.CrearadopcionPageModule),
+    canActivate: [authGuard]  // Protege la ruta 'crearadopcion'
   },
 ];
 
