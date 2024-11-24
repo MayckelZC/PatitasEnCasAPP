3a92c4865b3f0340c0bf4adda6b5a37b3084b7a7
Mayckel Zepeda <mayckelzc@gmail.com>
2024-11-24 16:27:26 -0300

    error404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,11 +45,19 @@ const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full',
   },
-
+  {
+    path: 'error404',
+    loadChildren: () => import('./pages/error404/error404.module').then(m => m.Error404PageModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'error404',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
